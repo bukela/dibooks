dc88f967204ab3d8418b9b9dc4a3f558c826558e
bukela <bukela@gmail.com>
2018-07-09 23:35:16 +0200

    bulma

--- app/Http/Controllers/WorkbookItemController.php
+++ app/Http/Controllers/WorkbookItemController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class WorkbookItemController extends Controller
 {
+    public function __construct() {
+
+        $this -> middleware('auth');
+        
+    }
     /**
      * Display a listing of the resource.
      *
